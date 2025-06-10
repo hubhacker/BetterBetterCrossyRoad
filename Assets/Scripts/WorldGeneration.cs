7e7492fee3060836388c8a774ec89a9fdfc7c2eb
lejubi <113481201+lejubi@users.noreply.github.com>
2025-06-09 22:05:21 -0400

    chicken doesnt tip over

--- Assets/Scripts/WorldGeneration.cs
+++ Assets/Scripts/WorldGeneration.cs
@@ -14,18 +14,14 @@ public class WorldGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Round down the chicken's Z position to the nearest whole unit
         int currentZ = Mathf.FloorToInt(chicken.position.z);
 
-        // Only generate a tile if chicken moved into a new unit
         if (currentZ > lastGeneratedZ)
         {
             lastGeneratedZ = currentZ;
 
-            // Choose a random tile prefab
             GameObject prefab = worldPrefabs[Random.Range(0, worldPrefabs.Length)];
 
-            // Spawn it 15 units ahead on the Z axis
             Vector3 spawnPosition = new Vector3(0, 0, currentZ + spawnOffset);
             Instantiate(prefab, spawnPosition, Quaternion.identity);
         }
